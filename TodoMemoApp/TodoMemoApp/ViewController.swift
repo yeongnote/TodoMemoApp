bbe83ec797edc011429f924cb31b10de03aaf208
yeongnote <eongh0640@gmail.com>
2024-01-08 20:06:38 +0900

    UI 코드 구현 환경설정

---
+++ TodoMemoApp/TodoMemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoMemoApp
+//
+//  Created by YeongHo Ha on 1/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+    }
+
+
+}
+
